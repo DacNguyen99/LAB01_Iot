eb45366d8065b5199c77bdba9b3707b79cad960a
DacNguyen99 <129793121+DacNguyen99@users.noreply.github.com>
2024-02-03 19:04:55 +0700

    init

---
+++ main.py
@@ -0,0 +1,47 @@
+import sys
+from Adafruit_IO import MQTTClient
+import time
+import random
+
+AIO_FEED_IDs = ["nutnhan1", "nutnhan2"]
+AIO_USERNAME = "datnguyenvan"
+AIO_KEY = "aio_aiEl12c7fXniluTpv3LOV9hZBttH"
+
+def connected(client):
+    print("Ket noi thanh cong ...")
+    for topic in AIO_FEED_IDs:
+        client.subscribe(topic)
+
+def subscribe(client , userdata , mid , granted_qos):
+    print("Subscribe thanh cong ...")
+
+def disconnected(client):
+    print("Ngat ket noi ...")
+    sys.exit (1)
+
+def message(client , feed_id , payload):
+    print("Nhan du lieu: " + payload + ", feed id:" + feed_id)
+
+client = MQTTClient(AIO_USERNAME , AIO_KEY)
+client.on_connect = connected
+client.on_disconnect = disconnected
+client.on_message = message
+client.on_subscribe = subscribe
+client.connect()
+client.loop_background()
+counter = 10
+
+while True:
+    counter = counter - 1
+    if counter <= 0:
+        counter = 10
+        print("Random data is being published...")
+        temp = random.randint(15, 60)
+        client.publish("cambien1", temp)
+        light = random.randint(0, 500)
+        client.publish("cambien2", light)
+        humid = random.randint(0, 100)
+        client.publish("cambien3", humid)
+
+    time.sleep(1)
+    pass
